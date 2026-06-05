a9a8c164a9159e9d01dda064b5e85cb215630160
giootrudu <g.trudu17@studenti.unica.it>
2026-06-05 11:57:12 +0200

    modello eventi database

--- app/models/event.py
+++ app/models/event.py
@@ -1,5 +1,12 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
+'''Rappresenta un evento nel sistema.
+Contiene informazioni su titolo, descrizione, data e luogo.
+'''
 class Event(SQLModel, table = True):
     event_id: int = Field(default = None, primary_key = True)
+    title: str
+    description: str
+    date: datetime
+    location: str
