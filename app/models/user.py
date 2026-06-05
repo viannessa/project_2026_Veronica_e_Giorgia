6f3df389952f96f04406f4eececc0a5abe7b74fc
veronicaiannessa <v.iannessa@studenti.unica.it>
2026-06-05 11:22:54 +0200

    modello user

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class User(SQLModel, table=True):
+    """
+    Modello per la tabella 'user'. Serve per memorizzare le info
+    degli utenti registrati alla piattaforma
+    """
+    username: str = Field(primary_key=True, index=True)
+    name: str
+    email: str
