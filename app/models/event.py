aef9ae9b2ba923550534aaf2d5b6343f1cf90a34
giootrudu <g.trudu17@studenti.unica.it>
2026-06-11 10:19:27 +0200

    API EVENTS

--- app/models/event.py
+++ app/models/event.py
@@ -15,7 +15,7 @@ class EventCreate (EventBase):
 class EventPublic(EventBase):
     id: int
 
-#3. MODELLO RELAZIONALE (DATABASE): crea la tabella ed è l'ORM vero e proprio
+#4. MODELLO RELAZIONALE (DATABASE): crea la tabella ed è l'ORM vero e proprio
 #è richiesto esplicitamente dall specifiche che si chiami "Event"
 class Event (EventBase, table = True):
     id: int = Field(default=None, primary_key=True)
