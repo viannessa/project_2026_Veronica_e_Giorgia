e732c661fd4b5405c4e2ae3704d96acf4c9fefaf
giootrudu <g.trudu17@studenti.unica.it>
2026-06-11 10:15:56 +0200

    API EVENTS

--- app/models/event.py
+++ app/models/event.py
@@ -1,12 +1,21 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
-'''Rappresenta un evento nel sistema.
-Contiene informazioni su titolo, descrizione, data e luogo.
-'''
-class Event(SQLModel, table = True):
-    event_id: int = Field(default = None, primary_key = True)
+#1. MODELLO BASE: USATO PER DEFINIRE I CAMPI COMUNI
+class EventBase(SQLModel):
     title: str
     description: str
     date: datetime
     location: str
+
+#2. MODELLO INPUT: USATO PER POST E PUT (no ID)
+class EventCreate (EventBase):
+    pass
+#3. MODELLO OUTPUT: USATO PER GET (con ID)
+class EventPublic(EventBase):
+    id: int
+
+#3. MODELLO RELAZIONALE (DATABASE): crea la tabella ed è l'ORM vero e proprio
+#è richiesto esplicitamente dall specifiche che si chiami "Event"
+class Event (EventBase, table = True):
+    id: int = Field(default=None, primary_key=True)
