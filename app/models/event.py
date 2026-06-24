83a0531498b4cd04972a516ff29c0c4413906ed6
giootrudu <g.trudu17@studenti.unica.it>
2026-06-24 18:27:21 +0200

    commenti

--- app/models/event.py
+++ app/models/event.py
@@ -4,7 +4,6 @@ from typing import Optional
 
 #1. MODELLO BASE: USATO PER DEFINIRE I CAMPI COMUNI
 class EventBase(SQLModel):
-    '''E' il modello di dati per le api con gli attributi comuni'''
     title: str
     description: str
     date: datetime
